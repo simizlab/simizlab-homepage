08121345dfa0948787f3e997db1df6c8f189738c
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2019-08-19 18:05:54 +0900

    add 2011 contents in ja

--- static/assets/generate_simple_markdown.bat
+++ static/assets/generate_simple_markdown.bat
@@ -4,7 +4,7 @@ set RUN_PY=%BASE_DIR%generate_simple_markdown.py
 
 ::==================
 set OUTPUT_DIR=I:\project\simizlab-homepages\test1\content\topics
-set GENERATE_DATE_LIST=2010-07-31 2010-08-28 2010-09-03 2010-09-16
+set GENERATE_DATE_LIST=2011-03-06 2011-05-19 2011-03-22 2011-07-17 2011-10-01 2011-12-01
 ::==================
 
 python %RUN_PY% -o %OUTPUT_DIR% -g %GENERATE_DATE_LIST%
