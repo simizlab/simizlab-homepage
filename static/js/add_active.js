be51556aa904a3f3e6c7d79fffb0a4ce27a4f4dc
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2019-10-09 15:23:41 +0900

    bug fix

--- static/js/add_active.js
+++ static/js/add_active.js
@@ -1,20 +1,24 @@
 // https://www.webantena.net/javascriptjquery/jquery-currentmenu-design/
 $('#navigation ul li a').each(function(){
-        var $href = $(this).attr('href');
-        var $current_href = location.href
+    if(location.href.match("topics")){
+      return;
+    }
 
-        if($href.slice(-1) == "/"){
-          var $href = $href.slice(0, -1);
-        }
-        if($current_href.slice(-1) == "/"){
-          var $current_href = $current_href.slice(0, -1);
-        }
+    var $href = $(this).attr('href');
+    var $current_href = location.href;
 
-        var $base = $(this).parent('.dropdown')
-        if($current_href == $href) {
-          $base.addClass('active');
-        }
-        else {
-          $base.removeClass('active');
-        }
+    if($href.slice(-1) == "/"){
+      var $href = $href.slice(0, -1);
+    }
+    if($current_href.slice(-1) == "/"){
+      var $current_href = $current_href.slice(0, -1);
+    }
+
+    var $base = $(this).parent('.dropdown')
+    if($current_href == $href) {
+      $base.addClass('active');
+    }
+    else {
+      $base.removeClass('active');
+    }
 });
